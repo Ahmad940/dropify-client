3e01e6d09bdc8d2bf4c7842c149e971f948b2ef3
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2023-09-30 14:55:46 +0100

    Initial: initial commit 🔥🚀

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+
+import './index.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
